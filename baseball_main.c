ebe4686f3d280396e3fc13c56314a99d3f736c2f
kyletho <k.hecker92@gmail.com>
2026-06-11 08:53:42 -0400

    making poisson and factorial functions

---
+++ baseball_main.c
@@ -0,0 +1,23 @@
+//
+//  baseball_main.c
+//  
+//
+//  Created by Kyle Hecker on 6/11/26.
+//
+
+#include <math.h>
+
+int factorial() {
+    
+}
+
+
+double poisson(double lambda, int x) {
+    
+    double e_value = exp(-lambda);
+    
+    double lamda_x = pow(lambda, x);
+    
+    
+    
+}
