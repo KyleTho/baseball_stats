096b3e2b0f622adfdc13a558e523ccc501edacb6
kyletho <k.hecker92@gmail.com>
2026-06-17 13:02:10 -0400

    adding repeat functionality

--- baseball_main.c
+++ baseball_main.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <errno.h>
+#include <string.h>
 
 
 int factorial(int n) {
@@ -39,12 +40,15 @@ int main() {
     int hr_predict;
     char buffer[50];
     char *endptr;
+    char answer[1];
     
-    printf("Please enter the average number of home runs for a specific \n"
-           "player for a specific time frame (e.g. a single game): ");
-    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
-        fprintf(stderr, "Input error\n");
+    while(1) {
         
+        printf("Please enter the average number of home runs for a specific \n"
+               "player for a specific time frame (e.g. a single game): ");
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+            fprintf(stderr, "Input error\n");
+        }
         errno = 0;
         
         home_runs = strtod(buffer, &endptr);
@@ -52,40 +56,45 @@ int main() {
             printf("Error: No valid number was found.\n");
         } else if (errno == ERANGE) {
             printf("Error: The number is too large or too small.\n");
-        } else {
-            printf("Successfully parsed number: %lf\n", home_runs);
-    }
-    
-    
-    printf("Please enter the number of home runs you would like to find the probability for: ");
-    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
-        fprintf(stderr, "Input error\n");
-            
-        errno = 0;
-            
-        home_runs = strtod(buffer, &endptr);
-        if (endptr == buffer) {
-            printf("Error: No valid number was found.\n");
-        } else if (errno == ERANGE) {
-            printf("Error: The number is too large or too small.\n");
-        } else {
-            printf("Successfully parsed number: %lf\n", home_runs);
-    }
-    
-    double poisson_count = poisson(home_runs, hr_predict);
-    double poisson_sum = 0;
-    
-    if (hr_predict > 0) {
-        for (int i=0; i < hr_predict; i++) {
-            poisson_sum += poisson(home_runs, i);
         }
         
-        double final_poisson = 1.0 - poisson_sum;
         
-        printf("The probability of the player hitting at least %d home run(s) is %lf", hr_predict, final_poisson);
+        printf("Please enter the number of home runs you would like to find the probability for: ");
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+            fprintf(stderr, "Input error\n");
+        }
+        errno = 0;
         
-    } else {
-        printf("The probability of the player hitting %d home runs is %lf", hr_predict, poisson_count);
+        hr_predict = strtol(buffer, &endptr, 10);
+        if (endptr == buffer) {
+            printf("Error: No valid number was found.\n");
+        } else if (errno == ERANGE) {
+            printf("Error: The number is too large or too small.\n");
+        }
+        
+        double poisson_count = poisson(home_runs, hr_predict);
+        double poisson_sum = 0;
+        
+        if (hr_predict > 0) {
+            for (int i=0; i < hr_predict; i++) {
+                poisson_sum += poisson(home_runs, i);
+            }
+            
+            double final_poisson = 1.0 - poisson_sum;
+            
+            printf("The probability of the player hitting at least %d home run(s) is %lf\n", hr_predict, final_poisson);
+            
+        } else {
+            printf("The probability of the player hitting %d home runs is %lf\n", hr_predict, poisson_count);
+        }
+        
+        printf("Would you like to run the calculation for another player? (y/n)");
+        if (fgets(answer, sizeof(answer), stdin) == NULL) {
+            fprintf(stderr, "Input error\n");
+        }
+        if (strcmp(answer, "n") == 0 || strcmp(answer, "N") == 0) {
+            break;
+        }
     }
     
     return 0;
